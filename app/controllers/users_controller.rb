25468ea01674d3d573051fadb4c4b3d52586ef9a
dowpm <mcdowpm@gmail.com>
2019-03-28 12:06:35 -0400

    add new and create actions and user_params

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "You have successfully registered and logged in."
+      render plain: flash[:notice]
+    #   redirect_to books_path
+    else 
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:full_name,:author,:email, :password, :password_confirmation)
+  end
+end
