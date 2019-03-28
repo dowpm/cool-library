d944efba1c17e5ad2fb79086fe6890d891f79fd4
dowpm <mcdowpm@gmail.com>
2019-03-28 15:32:41 -0400

    add find_or_create_by_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
     validates :password, presence: true, length: { in: 6..100 }
   
     has_secure_password
+
+    def self.find_or_create_by_omniauth(auth_hash)
+        self.where(:email => auth_hash["info"]["email"]).first_or_create do |user|
+            user.password = SecureRandom.hex
+        end
+    end
 end
