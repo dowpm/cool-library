abf686a10abe314bc03731f17ec962adda3f3ea7
dowpm <mcdowpm@gmail.com>
2019-03-28 14:55:19 -0400

    add login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/users', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
