58ef303c86a50e524a33e669c120103d183846da
dowpm <mcdowpm@gmail.com>
2019-03-28 15:26:50 -0400

    add auth/fb/cb route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  get '/auth/facebook/callback', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
