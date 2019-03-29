386253bfae0a0d1d1d9770ac5fdc47d73e02a2a6
dowpm <mcdowpm@gmail.com>
2019-03-29 12:32:34 -0400

    add helpers logged_in? current_user and require_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+    helper_method :logged_in?, :current_user, :require_login
+  
+    private
+
+    def logged_in?
+        !!current_user
+    end
+
+    def current_user
+        if session[:user_id]
+        @current_user ||= User.find_by(id: session[:user_id])
+        end
+    end
+
+    def require_login
+        unless logged_in?
+        flash[:error] = "You must be logged in to access this section."
+        redirect_to login_path
+        end
+    end
 end
