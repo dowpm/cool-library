5acf7b42ea3bdb3f16a01180b55a906d0a8e656b
dowpm <mcdowpm@gmail.com>
2019-03-29 11:26:42 -0400

    add show edit index and update actions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,15 @@
 class BooksController < ApplicationController
+    before_action :set_book, except: [:index, :create, :new]
+    
+    def index
+        # if params[:user_id]
+        #   @user = current_user
+        #   @books = @user.books
+        # else
+          @books = Book.all
+        # end
+    end
+
     def new
         @book = Book.new
     end
@@ -12,7 +23,41 @@ class BooksController < ApplicationController
         end
     end
 
+    def show
+        if !@book
+          flash[:notice] = "Book does not exist."
+          render plain: flash[:notice]
+        #   redirect_to books_path
+        end
+    end
+
+    def edit
+        # @user = current_user
+        if !@book
+          flash[:notice] = "book does not exist for this user."
+          render plain: flash[:notice]
+        #   redirect_to books_path
+        end
+    end
+
+    def update
+        if @book.update(book_params)
+            redirect_to book_path
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @book.delete
+        redirect_to books_path
+    end
+
     private
+
+    def set_book
+        @book = Book.find_by(id: params[:id])
+    end
 
     def book_params
         params.require(:book).permit( 
