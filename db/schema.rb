7255307f91b680a644660cb824feaf66b4732d94
dowpm <mcdowpm@gmail.com>
2019-03-28 23:02:29 -0400

    migrate books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_28_142809) do
+ActiveRecord::Schema.define(version: 2019_03_29_011256) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "isbn"
+    t.integer "nbr_page"
+    t.string "summary"
+    t.integer "publication_year"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
