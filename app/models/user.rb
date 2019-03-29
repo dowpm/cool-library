355094675c552748c5fdc6eaa3aeb727e51b4b76
dowpm <mcdowpm@gmail.com>
2019-03-28 20:36:27 -0400

    add full_name for omniauth user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
 
     def self.find_or_create_by_omniauth(auth_hash)
         self.where(:email => auth_hash["info"]["email"]).first_or_create do |user|
+            user.full_name = auth_hash["info"]["name"]
             user.password = SecureRandom.hex
         end
     end
