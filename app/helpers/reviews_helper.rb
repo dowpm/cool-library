496fc40b4de4b0192cc7fe83ea87f62cfebf340d
dowpm <mcdowpm@gmail.com>
2019-03-30 19:42:58 -0400

    create reviews controllerand helper

---
+++ app/helpers/reviews_helper.rb
@@ -0,0 +1,30 @@
+module ReviewsHelper
+    def show_stars(rating)
+        case rating
+        when 5
+            "⭐️""⭐️""⭐️""⭐️""⭐️"
+        when 4
+            "⭐️""⭐️""⭐️""⭐️"
+        when 3
+            "⭐️""⭐️""⭐️"
+        when 2
+            "⭐️""⭐️"
+        when 1
+            "⭐️"
+        else
+        "No ratings yet. Be the first?"
+        end
+    end
+
+    def book_id_field(review)
+        if review.book.nil?
+            select_tag "review[book_id]", options_from_collection_for_select(Book.all, :id, :name), :prompt => "Select a book"
+        else
+            hidden_field_tag "review[book_id]", review.book.id
+        end
+    end
+
+    def list_with_names(review)
+    "Book: #{review.wine.name} "
+    end
+end
