6f05fdefa9c41c38f0897027fd2df61593f2cedb
dowpm <mcdowpm@gmail.com>
2019-03-30 15:39:44 -0400

    add has_may reviewd bokks as comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
     has_many :books
-    
+
+    # has_many :reviewed_books, foreign_key: "user_id", class_name: "Review"
+    # has_many :reviewed_books, through: :reviews, -> { where(user_id: 1) }
+
     validates :email, presence: true, uniqueness: true
     validates :full_name, presence: true
     validates :password, presence: true, length: { in: 6..100 }
