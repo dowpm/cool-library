fec849e26cd0832cfb302635dcac12a3cb81f65a
dowpm <mcdowpm@gmail.com>
2019-03-30 19:44:43 -0400

    migrate reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_29_011256) do
+ActiveRecord::Schema.define(version: 2019_03_30_211003) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2019_03_29_011256) do
     t.string "summary"
     t.integer "publication_year"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.integer "stars"
+    t.text "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
