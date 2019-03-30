c743453f677cdc077c8c07f29d8982632fa8e656
dowpm <mcdowpm@gmail.com>
2019-03-30 15:38:59 -0400

    change year to publication_year and message

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,9 @@
 class Book < ApplicationRecord
     belongs_to :user
 
-    validates :year,
+    validates :publication_year,
         numericality: { only_integer: true },
-        inclusion: { in: 1900..Time.current.year },
+        inclusion: { in: 1900..Time.current.year, message: 'should be between 1900 and this year' },
         format: {
         with: /(19|20)\d{2}/i,
         message: "should be a four-digit year"
