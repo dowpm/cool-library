8aa049263132d1ec23811d33aa27f9dbf9798ab8
dowpm <mcdowpm@gmail.com>
2019-03-30 15:29:52 -0400

    add validation for year

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,14 @@
 class Book < ApplicationRecord
     belongs_to :user
 
+    validates :year,
+        numericality: { only_integer: true },
+        inclusion: { in: 1900..Time.current.year },
+        format: {
+        with: /(19|20)\d{2}/i,
+        message: "should be a four-digit year"
+        }
+
     def author_name
         user.full_name
     end
