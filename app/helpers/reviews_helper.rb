d4d4f9d30f8b8a236821e79b18cd7236e1616ac9
dowpm <mcdowpm@gmail.com>
2019-03-30 22:30:14 -0400

    comment unuse methods

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -16,15 +16,15 @@ module ReviewsHelper
         end
     end
 
-    def book_id_field(review)
-        if review.book.nil?
-            select_tag "review[book_id]", options_from_collection_for_select(Book.all, :id, :name), :prompt => "Select a book"
-        else
-            hidden_field_tag "review[book_id]", review.book.id
-        end
-    end
+    # def book_id_field(review)
+    #     if review.book.nil?
+    #         select_tag "review[book_id]", options_from_collection_for_select(Book.all, :id, :name), :prompt => "Select a book"
+    #     else
+    #         hidden_field_tag "review[book_id]", review.book.id
+    #     end
+    # end
 
-    def list_with_names(review)
-    "Book: #{review.wine.name} "
-    end
+    # def list_with_names(review)
+    # "Book: #{review.book.title} "
+    # end
 end
