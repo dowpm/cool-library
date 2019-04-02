3d0c965975e05aacec09093b08fd27f78b56aa2b
dowpm <mcdowpm@gmail.com>
2019-04-02 14:07:30 -0400

    edit flash for alert msg and change the render to redirection route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,15 +9,15 @@ class SessionsController < ApplicationController
             @user = User.find_or_create_by_omniauth(auth_hash)
             session[:user_id] = @user.id
             flash[:notice] = "You're logged in."
-            render plain: flash[:notice]
-            # redirect_to books_path
+            # render plain: flash[:notice]
+            redirect_to user_path @user
         else
             @user = User.find_by(email: params[:email])
             if @user && @user.authenticate(params[:password])
                 session[:user_id] = @user.id
                 flash[:notice] = "You're logged in."
-                render plain: flash[:notice]
-                # redirect_to books_path
+                # render plain: flash[:notice]
+                redirect_to user_path @user
             else 
                 flash[:alert] = "Invalid username/password."
                 render :new
@@ -27,7 +27,7 @@ class SessionsController < ApplicationController
 
     def destroy
         session.clear unless session[:user_id].nil?
-        flash[:notice] = "You have successfully logged out."
+        flash["alert alert-info"] = "You have successfully logged out."
         redirect_to login_path
     end
 
